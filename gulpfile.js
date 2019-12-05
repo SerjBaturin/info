278e5fa592bbe73dfd9ccd6c9c2b2476ded56e7a
Serj Baturin <sergejbaturin1985@gmail.com>
2019-12-05 09:03:58 +0500

    Tools: add gulp & browser-sync - INITIAL COMMIT

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const browserSync = require('browser-sync').create()
+
+function css () {
+  return gulp.src('./*.scss')
+  .pipe(sass())
+  .pipe(autoprefixer('last 2 versions'))
+  .pipe(gulp.dest('./'))
+}
+
+function watch () {
+  browserSync.init({
+    server: './'
+  })
+
+  gulp.watch('./*.scss', css)
+  gulp.watch('./index.html').on('change', browserSync.reload)
+  gulp.watch('./js/js.js').on('change', browserSync.reload)
+  gulp.watch('./*.scss').on('change', browserSync.reload)
+}
+
+exports.css = css
+exports.watch = watch
